2839e2f50213fea704d30c9e5bca7296211931fa
ssr <ssrssr0095@gmail.com>
2025-02-24 01:52:21 +0530

    fix: db url

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -6,7 +6,7 @@ const connectDB = async () => {
   });
 
   // await mongoose.connect(`${process.env.MONGODB_URI}/e-commerce`);
-  await mongoose.connect("mongodb://localhost:27017/e-commerce");
+  await mongoose.connect(process.env.MONGODB_URI);
 };
 
 export default connectDB;
