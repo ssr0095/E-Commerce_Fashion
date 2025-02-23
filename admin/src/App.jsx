b78f8bff9a340cc4849070b5d600bc4783de1c3b
ssr <ssrssr0095@gmail.com>
2025-02-24 02:04:40 +0530

    fix: lazy loader

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -9,6 +9,7 @@ import Login from "./components/Login";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Loading from "./components/Loading";
+import { lazy } from "react";
 
 export const backendUrl = import.meta.env.VITE_BACKEND_URL;
 export const currency = "₹";
