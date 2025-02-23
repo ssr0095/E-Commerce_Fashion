93347cadb1161a124dc24dca38cebbe1cd02d3d8
ssr <ssrssr0095@gmail.com>
2025-02-24 01:41:30 +0530

    fixed: vs.config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,9 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
+  rollupOptions: {
+    external: ["qrcode.react"],
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
