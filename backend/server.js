669607483494dad81222e5f63852692a8e0bdfa8
Sriram Subramani <ssrssr0095@gmail.com>
2025-03-09 22:24:01 +0530

    fix: cors

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ connectCloudinary()
 
 // middlewares
 app.use(express.json())
-app.use(cors())
+app.use(cors({ origin: "*", methods: ["GET", "POST", "PUT", "DELETE"] }));
 
 // api endpoints
 app.use('/api/user',userRouter)
